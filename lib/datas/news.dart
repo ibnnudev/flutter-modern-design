f94c77c1095accc9591febe9565a444282fa1220
Moh Ibnu Abdurrohman Sutio <moh.ibnu@admo.astra.co.id>
2026-02-24 03:38:45 +0700

    feat: add GojekIcon and News models, along with gopayIcons and news data

---
+++ lib/datas/news.dart
@@ -0,0 +1,7 @@
+import 'package:moderndesign/models/news.dart';
+
+List<News> news = [
+  News(image: '1.png', title: 'Makin Seru 😉', description: 'Aktifkan & Sambungkan GoPay & GoPayLater di Tokopedia'),
+  News(image: '1.png', title: 'Makin Seru 😉', description: 'Sambungin Akun ke Tokopedia, Banyakin Untung'),
+  News(image: '1.png', title: 'Makin Seru 😉', description: 'Promo Belanja Online 10.10: Cashback hingga Rp100.000'),
+];
