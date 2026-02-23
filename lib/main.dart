a2515ce6ac84b3ceffd343321ae74c41401d4d32
Moh Ibnu Abdurrohman Sutio <moh.ibnu@admo.astra.co.id>
2026-02-23 17:51:05 +0700

    setup: assets

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:moderndesign/pages/first.dart';
-import 'package:moderndesign/pages/second.dart';
 
 void main() {
   runApp(SevenDays());
@@ -10,7 +7,7 @@ void main() {
 class SevenDays extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Second(),
+      home: Scaffold(),
       debugShowCheckedModeBanner: false,
     );
   }
