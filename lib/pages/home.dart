39239f48dec751e9d9aae3a43c4c913a45de4615
Moh Ibnu Abdurrohman Sutio <moh.ibnu@admo.astra.co.id>
2026-02-24 04:09:26 +0700

    feat: enhance HomePage layout with search and profile components

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:moderndesign/theme.dart';
 
 class HomePage extends StatelessWidget {
@@ -7,6 +8,7 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
         backgroundColor: green2,
         elevation: 0,
@@ -52,6 +54,79 @@ class HomePage extends StatelessWidget {
               ),
             ],
           ),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 23, left: 15, right: 15),
+              child: Row(
+                children: [
+                  // Search
+                  Flexible(
+                    fit: FlexFit.tight,
+                    child: Container(
+                      padding: const EdgeInsets.all(8.0),
+                      decoration: BoxDecoration(
+                        color: dark4,
+                        borderRadius: BorderRadius.all(Radius.circular(30)),
+                        border: Border.all(color: Color(0xFF0E8E8E8)),
+                      ),
+                      child: Row(
+                        children: [
+                          SvgPicture.asset(
+                            'assets/icons/search.svg',
+                            colorFilter: ColorFilter.mode(
+                              dark2,
+                              BlendMode.srcIn,
+                            ),
+                            width: 20,
+                            height: 20,
+                          ),
+                          SizedBox(width: 10),
+                          Text(
+                            'Cari layanan, makanan & tujuan',
+                            style: regular14.copyWith(color: dark3),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: 15),
+                  // Profile
+                  Container(
+                    width: 35,
+                    height: 35,
+                    child: Stack(
+                      children: [
+                        Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(35/2),
+                          ),
+                          clipBehavior: Clip.hardEdge,
+                          child: Image.asset('assets/images/avatar.png'),
+                        ),
+                        Positioned(
+                          right: 0,
+                          bottom: 0,
+                          child: Container(
+                          width: 16,
+                          height: 16,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(35/2),
+                            color: const Color(0xFFD1E7EE)
+                          ),
+                          clipBehavior: Clip.hardEdge,
+                          child: SvgPicture.asset('assets/icons/goclub.svg', color: blue2,),
+                        ))
+                      ],
+                    ), 
+                  )
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
